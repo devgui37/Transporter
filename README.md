a8599334652da5620e7e867803d588772b5d93f1
devgui37 <100968158+devgui37@users.noreply.github.com>
2023-05-01 15:40:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
-# Transporter
+# Sujet 06 : transport optimal
+
+Le but de ce projet est de minimiser le coût de transport d'une entreprise disposant de plusieurs entrepots cherchant à deservir plusieurs clients.
+
+## Méthode utilisé
+Cette minimisation de coût sera effectué à l'aide de la fonction `linprog` du package [scipy](https://scipy.org/). 
+En effet, pour ce problème on minimise le coût total de distribution de tous les clients en fonction des différentes contrainte de stock d'entrepots et des demandes des clients. 
+
+Les contraintes sont les suivantes: 
+- $\sum q_{ij} \le Q_i$ avec $Q_i$ la quantité total disponible dans l'entrepôt $i$. 
+- $\sum q_{ij} = D_j$ avec $D_j$ la quantité total demandé par le client $j$.
+
+
+## Installation / fichier requis
+Tous les fichiers présent dans le repot GitHub.
+
+
+## La base de données
+La résolution du problème ne fonctionne qu'avec un type spécifique de base de données.
+
+- Un base de données enregistré dans un fichier CSV.
+- Les quantités disponibles et demandées doivent être renseignée en tonne: "`xT`".
+- Les coûts doivent être de la forme : "`x$/T`".
+
+
+<details>
+  <summary>📷 Exemple de base de données </summary>
+  <hr>
+
+```
+,19T,20T,19T,16T,19T,
+27T,5$/T,3$/T,4$/T,5$/T,3$/T
+18T,1$/T,4$/T,2$/T,1$/T,1$/T
+22T,4$/T,3$/T,4$/T,3$/T,5$/T
+25T,2$/T,3$/T,4$/T,3$/T,3$/T
+22T,4$/T,1$/T,3$/T,4$/T,2$/T
+```
+
+ </details>
+
+
+## Application avec Interface Utilisateur Textuel / dans le Terminal
+
+A l'aide du package [Typer](https://typer.tiangolo.com/). 
+
+- Pour voir l'aide. 
+```python
+python app.py --help
+```
+
+<details>
+  <summary>📷  Les différentes sorties </summary>
+  <hr>
+
+  Différents tableaux en sortie [Rich](https://github.com/Textualize/rich)
+
+```python
+python app.py resultat-client --chemin /chemin/vers/base_de_donnees.csv
+```
+<img
+  src="exemple/resultat_client.png"
+  style="display: inline-block;
+  align: center; 
+  margin: 0 auto; max-width: 500px">
+
+```python
+python app.py resultat-entrepot --chemin /chemin/vers/base_de_donnees.csv
+```
+<img
+  src="exemple/resultat_entrepot.png"
+  style="display: inline-block;
+  align: center; 
+  margin: 0 auto; max-width: 500px">
+
+  
+  Différente visualisation graphique à l'aide de graph [NetworkX](https://networkx.org/documentation/stable/index.html)
+
+```python
+python app.py representation-client --chemin ./transport.csv --client 30
+```
+<img
+  src="exemple/graph_client.png"
+  style="display: inline-block;
+  align: center; 
+  margin: 0 auto; max-width: 500px">
+
+```python
+python app.py representation-entrepot --chemin ./transport.csv --entrepot 30
+```
+<img
+  src="exemple/graph_entrepot.png"
+  style="display: inline-block;
+  align: center; 
+  margin: 0 auto; max-width: 500px">
+
+
+ </details>
+
+
+## Application avec Interface Utilisateur Graphique. 
+
+A l'aide du package [Textual](https://github.com/Textualize/textual).
+
+```
+textual run app_textual.py
+```
+Tout ce passe à la souris (ou presque !).
+
+<details>
+  <summary>🎬 Démonstration </summary>
+  <hr>
+
+
+https://user-images.githubusercontent.com/125368997/235438571-7049c92c-37ae-4eff-885d-174208d8a4fc.mp4
+
+  
+ </details>
+
+## Compatibilité
+
+Compatible avec Windows. 
+
+Compatibilité avec Linux et MacOS à tester. 
+
+Pour les packages voir fichier `pyproject.toml`. 
---
+++ app_textual.py
@@ -0,0 +1,152 @@
+import sys
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Static, Button, DirectoryTree, Label, Input, Footer
+from textual.containers import  VerticalScroll, Horizontal, Grid
+from textual.reactive import var, reactive
+from lib.lib_graphe import *
+from lib.lib_transport import *
+from lib.lib_format import *
+
+
+class QuitScreen(Screen):
+    """Ecran qui nous permet de quitter l'application."""
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Label("Voulez vous vraiment quitter l'application ?", id="question"),
+            Button("Quitter", variant="error", id="quit"),
+            Button("Annuler", variant="primary", id="cancel"),
+            id="dialog",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Action suivant l'interaction avec les boutons."""
+        if event.button.id == "quit":
+            self.app.exit()
+        else:
+            self.app.pop_screen()
+
+
+class ErrorScreen(Screen):
+    """Ecran qui apparait lors d'une erreur et qui affiche celle-ci."""
+    ...
+    #en developpement
+
+
+
+class ResultScreen(Screen):
+    """Ecran qui donne le résultat de la commande choisie."""
+    def __init__(self, content: str):
+        self.content = content
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Horizontal(
+            VerticalScroll(Label(self.content), id="left"),
+            VerticalScroll(
+                Grid(
+                    Label("Voulez-vous continuer ?", id="question"),
+                    Button("Oui", variant="success", id="cancel"),
+                    Button("Non", variant="error", id="quit"),
+                    id="dialog"), 
+            id="right"
+            )
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Action suivant l'interaction avec les boutons."""
+        if event.button.id == "quit":
+            self.app.exit()
+        else:
+            self.app.pop_screen()
+
+
+
+class MyApp(App):
+    """L'application qui permet d'executer les différentes commandes."""
+
+    CSS_PATH = "aes.css"
+
+    chemin = var(str)
+    sortie = var(str)
+    show_tree = var(True)
+    num_c = reactive(1)
+    num_e = reactive(1)
+
+
+    BINDINGS = [
+        ("q", "request_quit", "Quitter l'application")
+    ]
+
+
+    def compose(self) -> ComposeResult:
+        path = "./" if len(sys.argv) < 2 else sys.argv[1] 
+        yield Horizontal(
+            VerticalScroll(DirectoryTree(path, id="tree-view")),
+            VerticalScroll(Static("Les commandes:", classes="header"),
+                           Button("Total coût", id="total_c"),
+                           Button("Total quantité", id = "total_q"),
+                           Button("Représentation", id = "rep")
+                           ),
+            VerticalScroll(Static("Client", classes="header"),
+                           Button.success("Résultat client", id="res_client"),
+                           Button.success("Représentation client", id = "rep_client"),
+                           Input(placeholder="Numéro du Client", id="num_client")
+                           ),
+            VerticalScroll(Static("Entrepôt", classes="header"),
+                           Button("Résultat entrepot", variant="primary", id="res_entrepot"),
+                           Button("Représentation entrepot", variant="primary", id = "rep_entrepot"),
+                           Input(placeholder="Numéro de l'entrepot", id="num_entrepot")
+                           ),
+                           )
+        yield Footer()
+
+
+    def on_directory_tree_file_selected(self, event: DirectoryTree.FileSelected):
+        """Après sélection d'un fichier."""
+        self.chemin = event.path
+
+    def on_input_changed(self, event: Input.Changed) -> None:  
+        """Lorsque la valeur des inputbox change, change le graphe demandé."""
+        input_id = event.input.id
+        if input_id == "num_client":
+            self.num_c = int(event.value or "1")
+        if input_id == "num_entrepot":
+            self.num_e = int(event.value or "1")
+        
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Lorsque l'on appuie sur l'un des boutons de l'accueil, lance la commande en question."""
+        button_id = event.button.id
+        assert button_id is not None
+
+        if button_id == "total_c":
+            content = "Le coût total minimal est de " + str(total_cout(lt.import_data(self.chemin))) + "$."
+            self.push_screen(ResultScreen(content))
+        elif button_id == "total_q":
+            content = "La quantité livré est " + str(total_res(lt.import_data(self.chemin))) + "."
+            self.push_screen(ResultScreen(content))
+        elif button_id == "rep":
+            self.sortie = graphe(lt.import_data(self.chemin))
+        elif button_id == "res_client":
+            content = client_res(lt.import_data(self.chemin))
+            self.push_screen(ResultScreen(content))
+        elif button_id == "res_entrepot":
+            content = entrepot_res(lt.import_data(self.chemin))
+            self.push_screen(ResultScreen(content))
+        elif button_id == "rep_client":
+            self.sortie = graphe_client(lt.import_data(self.chemin), client=self.num_c)
+        elif button_id == "rep_entrepot":
+            self.sortie = graphe_entrepot(lt.import_data(self.chemin), entrepot=self.num_e)
+        #rajouter le fait que si il y a une erreur, erreur = msg.erreur
+
+    
+    def action_request_quit(self) -> None:
+        """Lorsque l'utilisateur appuie sur la touche Q ou clique sur "Quitter l'application", lance l'écran qui permet de quitter l'application."""
+        self.push_screen(QuitScreen())
+
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.run()
